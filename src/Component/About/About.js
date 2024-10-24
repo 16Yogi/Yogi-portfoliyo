376a2a5b4757e89d40a20317420db258f8789565
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-10-24 17:12:13 +0530

    Commit
    Commit

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.scss'
+import resume from '../../Pdf/Yogendra-Meravi.pdf'
 export default function About1() {
   return (
     <>
@@ -10,7 +11,7 @@ export default function About1() {
                     <p>I am Yogendra Maravi. I am from Bhopal MP. I have done my schooling from JDES Hr. Sec. School. I have done my UG in Software Development from IGNTU Amarkantak MP. currently I am pursuing Master in Computer Application(MCA) from IGNTU, Amarakantak. </p>
                     <p>I am always excited to learn new things. I have skills and knowledge in development, problem solving. These things make my mind fresh and mood happy and that's the reason I do development.</p>
                     <p>Basically I am full stack developer I have done some freelancing projects and also some personal projects from scratch, which you can see in Github page. I've done some projects for companies like Beangate Academy. I've done my intership in both offline and online site medium. This has given me a holistic idea of development. To put everything in a nutshell I'm a kind of person who wants to keep evolving, sharpening his skills and becoming a better person.</p>
-                    <a href='/'><button className='btn btn-info'>My Resume</button></a>
+                    <a href={resume}><button className='btn btn-info'>My Resume</button></a>
                 </div>
             </div>
        </div>
--- src/Component/Worktogether/Workstogether.js
+++ src/Component/Worktogether/Workstogether.js
@@ -1,87 +1,102 @@
-import React, { useState } from 'react'
-import './Worktogether.scss'
+import React, { useRef, useState } from 'react';
+import './Worktogether.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Workstogether() {
-  const [name,setName] = useState("");
-  const [email,setEmail] = useState("");
-  const [mobile,setMobile] = useState("");
-  const [comment,setComment] = useState("");
+  const form = useRef(); // Define the form reference here
+  const [buttonText, setButtonText] = useState('Send Email'); // Button text state
 
-  const senddata = async()=>{
-       try{
-          const user = {
-            name:name,
-            email:email,
-            mobile:mobile,
-            comment:comment
-          };
-          const data = await fetch("http://localhost:5000/users",{
-            method:"post",
-            headers:{
-              "Content-Type":"application/json",
-            },
-            body:JSON.stringify(user),
-          });
-        }    catch{}
-  };
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setButtonText('Sending...'); // Change button text when email is sending
 
-  const name1 = (e) =>{
-    const value = e.target.value;
-    setName(value);
-  };
-
-  const email1 = (e) =>{
-    const value = e.target.value;
-    setEmail(value);
-  };
-  
-  const mobile1 = (e) =>{
-    const value = e.target.value;
-    setMobile(value);
-  };
-
-  const comment1 = (e) =>{
-    const value = e.target.value;
-    setComment(value);
+    emailjs
+      .sendForm(
+        'service_y1kqg9t', // Your service ID
+        'template_m50xumc', // Your template ID
+        form.current, // Form reference
+        'yg0fiHD_DMPbD6blQ' // Your public key
+      )
+      .then(() => {
+        setButtonText('Send Email'); // Reset button text after success
+        alert('Email successfully sent!');
+      })
+      .catch((error) => {
+        setButtonText('Send Email'); // Reset button text on error
+        alert('Email not sent, please try again. ' + JSON.stringify(error));
+      });
   };
 
   return (
     <>
-        <div className='container-fluid' id="worktogether-c-f">
-            <div className='container'>
-                {/* <div className='row'> */}
-                    <div className='col'>
-                        <h3>Let's Work Together</h3>
-                    </div>
-                    <div className='col p-5 mx-auto' id="worktogether-wrapper">
-                        <form action="" method=''>
-                            <div class="form-group">
-                              <label for="username">Full Name:</label>
-                              <input type="text" class="form-control" placeholder="Full Name" id="username" name="name" onChange={name1} value={name}/>
-                            </div>
-                            <div class="form-group">
-                              <label for="email">Email Address:</label>
-                              <input type="email" class="form-control" placeholder="Email" id="email" name="email" onChange={email1} value={email}/>
-                            </div>
-                            <div class="form-group">
-                              <label for="mobile">Mobile:</label>
-                              <input type="mobile" class="form-control" placeholder="Mobile Number" id="mobile" onChange={mobile1} value={mobile}/>
-                            </div>
-                            <div class="form-group">
-                              <label for="comment">Comment:</label>
-                              <textarea class="form-control" rows="5" id="comment" name="comment" onChange={comment1} value={comment}></textarea>
-                            </div>
-                            {/* <div class="form-group form-check">
-                              <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox"/> Remember me
-                              </label>
-                            </div> */}
-                            <button type="submit" class="btn btn-info" name='submit' onClick={senddata}>Submit</button>
-                        </form>
-                    </div>
-                {/* </div> */}
-            </div>
+      <div className='container-fluid' id="worktogether-c-f">
+        <div className='container'>
+          <div className='col'>
+            <h3>Let's Work Together</h3>
+          </div>
+          <div className='col p-5 mx-auto' id="worktogether-wrapper">
+            <form ref={form} onSubmit={sendEmail}> {/* Attach the ref to the form */}
+              <div className="form-group">
+                <label htmlFor="username">Full Name:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Full Name"
+                  id="username"
+                  name="from_name" // Change to match the name field in your EmailJS template
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="to_name">To Name:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="To Name"
+                  id="to_name"
+                  name="to_name" // Make sure this matches the EmailJS template variable
+                  defaultValue="Yogendra" // You can set a default value here
+                  readOnly // Prevent user from changing this
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email Address:</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  id="email"
+                  name="reply_to" // Reply_to field for EmailJS
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="mobile">Mobile:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Mobile Number"
+                  id="mobile"
+                  name="mobile"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Comment:</label>
+                <textarea
+                  className="form-control"
+                  rows="5"
+                  id="message"
+                  name="message" // Message field for EmailJS
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-info" name="submit">
+                {buttonText} {/* Dynamic button text */}
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
     </>
-    )
+  );
 }
